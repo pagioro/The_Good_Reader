da131368c59140bd1f1a4001d89e3f5ac8e39b93
pagioro <pagioro@hotmail.com>
2022-12-14 11:16:48 +0000

    Added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51MEsU0EgU9hOwUitsEsyB5hzgxi1CAbrGzUYiRuEwTxoq8J7tCswz7ZfuWR5tmlwIJJ6B4aKPd2KC9Vd49v91Z0I00KcxAwxmo',
+        'client_secret': 'test client secret',       
     }
 
     return render(request, template, context)
