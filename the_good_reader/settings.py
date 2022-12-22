311397c238d2393f469f149d1a022d6cf00e6503
pagioro <pagioro@hotmail.com>
2022-12-22 10:51:45 +0000

    Started the deployment project

--- the_good_reader/settings.py
+++ the_good_reader/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 
 """
 Django settings for the_good_reader project.
@@ -27,7 +28,7 @@ SECRET_KEY = 'django-insecure-s0s%=sx3n%(ajd5=-p4=jdb-mcz#2-0!c9b5&qqqr2wt^wp=nv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['the-good-reader.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -111,12 +112,17 @@ WSGI_APPLICATION = 'the_good_reader.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
