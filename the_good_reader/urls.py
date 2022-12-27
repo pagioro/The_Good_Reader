d9b2a64f9ec594e263a5cdda9d84518277921162
pagioro <pagioro@hotmail.com>
2022-12-27 20:25:20 +0000

    Added contatc and wishlist in settings.py

--- the_good_reader/urls.py
+++ the_good_reader/urls.py
@@ -29,6 +29,8 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('authors/', include('author.urls')),
     path('profile/', include('profiles.urls')),
+    path('contact/', include('contact.urls')),
+    path('wishlist/', include('wishlist.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'the_good_reader.views.handler404'
