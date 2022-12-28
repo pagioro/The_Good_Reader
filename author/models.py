06b5bd743dca835342ae55f921a80e8493badca4
pagioro <pagioro@hotmail.com>
2022-12-28 16:25:50 +0000

    Added image fildes in Author models

--- author/models.py
+++ author/models.py
@@ -6,3 +6,5 @@ class Author(models.Model):
     bio = models.CharField(max_length=254)
     known_for = models.CharField(max_length=254)
     image_url = models.CharField(max_length=254)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
