98a77599b5d286e25d0ef1ed9c4286d7f2e66cd2
pagioro <pagioro@hotmail.com>
2023-03-08 17:50:02 +0000

    fixing superuser bugs

--- env.py
+++ env.py
@@ -3,3 +3,4 @@ import os
 os.environ['STRIPE_PUBLIC_KEY']='pk_test_51MEsU0EgU9hOwUitsEsyB5hzgxi1CAbrGzUYiRuEwTxoq8J7tCswz7ZfuWR5tmlwIJJ6B4aKPd2KC9Vd49v91Z0I00KcxAwxmo'
 os.environ['STRIPE_SECRET_KEY']='sk_test_51MEsU0EgU9hOwUitKQQsaqp1jJ1kY7B6vMX4Pzx3PvCpi49G2XDxJtUzTe9iGZ15GZ4A0NDLs9dvUQRQhNiPv9Uz00QPZOkBhj'
 os.environ['SECRET_KEY']='18FaVCLeaxg37m1TGspDRBPusl/tGCW2QQEM5OzEVI4='
+os.environ['DATABASE_URL']='postgres://vdvnjmed:ENmbsJtCM6_pggMWuBpPavEYDvwxp51N@mel.db.elephantsql.com/vdvnjmed'
