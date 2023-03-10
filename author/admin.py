979494e13626d4539eaf5ba2472f227058748023
pagioro <pagioro@hotmail.com>
2023-03-10 15:16:58 +0000

    Fixing pep8 authors folder

--- author/admin.py
+++ author/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from .models import Author
 
+
 class AuthorAdmin(admin.ModelAdmin):
     list_display = (
         'name',
     )
+
 
 admin.site.register(Author, AuthorAdmin)
--- author/views.py
+++ author/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from .models import Author
 
+
 def get_authors(request):
     """ A view to show all products, including sorting and search queries """
 
-    authors = Author.objects.all()   
-    
+    authors = Author.objects.all()
+
     context = {
         'authors': authors,
     }
 
     return render(request, 'author/author.html', context)
+    
