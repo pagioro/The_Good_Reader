df52e15e0cd521eddcd8604921eb82a137d7cc32
pagioro <pagioro@hotmail.com>
2023-03-16 12:46:40 +0000

    changing env file

--- env.py
+++ env.py
@@ -4,3 +4,5 @@ os.environ['STRIPE_PUBLIC_KEY']='pk_test_51MEsU0EgU9hOwUitsEsyB5hzgxi1CAbrGzUYiR
 os.environ['STRIPE_SECRET_KEY']='sk_test_51MEsU0EgU9hOwUitKQQsaqp1jJ1kY7B6vMX4Pzx3PvCpi49G2XDxJtUzTe9iGZ15GZ4A0NDLs9dvUQRQhNiPv9Uz00QPZOkBhj'
 os.environ['SECRET_KEY']='18FaVCLeaxg37m1TGspDRBPusl/tGCW2QQEM5OzEVI4='
 os.environ['DATABASE_URL']='postgres://vdvnjmed:ENmbsJtCM6_pggMWuBpPavEYDvwxp51N@mel.db.elephantsql.com/vdvnjmed'
+os.environ['EMAIL_HOST_USER']='pagioropari@gmail.com'
+os.environ['EMAIL_HOST_PASS']='glkaruhsyffatynk'
