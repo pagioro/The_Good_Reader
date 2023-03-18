b6e82fd774364d12cdc11fe51b9da7d3cddfe4b5
pagioro <pagioro@hotmail.com>
2023-03-18 14:58:17 +0000

    fixing e-mail backend in settings.py

--- the_good_reader/settings.py
+++ the_good_reader/settings.py
@@ -216,10 +216,6 @@ STRIPE_PUBLIC_KEY = os.environ.get("STRIPE_PUBLIC_KEY")
 STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY")
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
-# # if 'DEVELOPMENT' in os.environ:
-# #     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-# #     # DEFAULT_FROM_EMAIL = 'the-good-reader@example.com'
-# else:
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
